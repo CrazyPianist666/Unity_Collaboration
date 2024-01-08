5000694dcff76bef3f062368e114321d03a3d276
CrazyPianist666 <151561340+CrazyPianist666@users.noreply.github.com>
2024-01-08 11:13:55 +0530

    Changes in Player & Camera Movement

--- Project_Game_Jam/Assets/scripts/Player_Movement.cs
+++ Project_Game_Jam/Assets/scripts/Player_Movement.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 public class Player_Movement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float forward_force = 1000f;
+    public float Movement_force = 1000f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(0, 0, forward_force * Time.deltaTime);
+            rb.AddForce(0, 0, Movement_force * Time.deltaTime);
         }
-       
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -Movement_force * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-Movement_force * Time.deltaTime, 0, 0 );
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Movement_force * Time.deltaTime, 0, 0);
+        }
+
     }
 }

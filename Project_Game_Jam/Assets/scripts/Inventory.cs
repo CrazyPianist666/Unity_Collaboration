9eb6076f69f67b95801224a0a36b8d565223b04c
Ashok0010001 <ashokmanivannan3@gmail.com>
2024-01-09 20:02:13 +0530

    Added Sprint

---
+++ Project_Game_Jam/Assets/scripts/Inventory.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using static UnityEditor.Progress;
+
+public class Inventory : MonoBehaviour
+{
+    [Header("UI")]
+    public GameObject inventory;
+    public List<Slot> InventorySlots = new List<Slot>();
+    public Image crosshair;
+    public TMP_Text itemHoverText;
+
+    [Header("Raycast")]
+    public float raycastDistance = 5f;
+    public LayerMask itemLayer;
+
+    public void Start()
+    {
+        toggleInventory(false);
+
+        foreach (Slot uiSlot in InventorySlots)
+        {
+            uiSlot.initialiseSlot();
+        }
+    }
+
+    public void Update()
+    {
+        itemRaycast(Input.GetMouseButtonDown(0));
+
+        if (Input.GetKeyDown(KeyCode.E))
+            toggleInventory(!inventory.activeInHierarchy);
+    }
+
+<<<<<<< HEAD
+    private void itemRaycast(bool hasClicked = false)
+    {
+        itemHoverText.text = "";
+
+        Ray ray = Camera.main.ScreenPointToRay(crosshair.transform.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, raycastDistance, itemLayer))
+        {
+            if (hit.collider != null)
+            {
+                if (hasClicked) // Pick up
+                {
+                    Item newItem = hit.collider.GetComponent<Item>();
+                    if (newItem)
+                    {
+                        addItemToInventory(newItem);
+                    }
+                }
+                else // Get the name
+                {
+                    Item newItem = hit.collider.GetComponent<Item>();
+
+                    if (newItem)
+                    {
+                        itemHoverText.text = newItem.name;
+                    }
+=======
+ private void ItemRaycast(bool hasClicked = false)
+{
+    itemHoverText.text = "";
+
+    if (mainCamera == null || playerTransform == null)
+    {
+        Debug.LogError("Main camera or player transform not assigned in the Inspector!");
+        return;
+    }
+
+    Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+    RaycastHit hit;
+
+    Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.red, 0.1f); // Draw the ray for debugging
+
+    if (Physics.Raycast(ray, out hit, raycastDistance, itemLayer))
+    {
+        if (hit.collider != null)
+        {
+            if (hasClicked) // Pick up
+            {
+                Item newItem = hit.collider.GetComponent<Item>();
+                if (newItem)
+                {
+                    AddItemToInventory(newItem);
+                }
+            }
+            else // Get the name
+            {
+                Item newItem = hit.collider.GetComponent<Item>();
+                if (newItem)
+                {
+                    itemHoverText.text = newItem.name;
+>>>>>>> 1b6b365bbe2e92ce6b01e72a0cf78a020d476d1e
+                }
+            }
+        }
+    }
+<<<<<<< HEAD
+=======
+    else
+    {
+        Debug.Log("Raycast did not hit anything.");
+    }
+}
+>>>>>>> 1b6b365bbe2e92ce6b01e72a0cf78a020d476d1e
+
+    private void addItemToInventory(Item itemToAdd)
+    {
+        int leftoverQuantity = itemToAdd.currentQuantity;
+        Slot openSlot = null;
+        for (int i = 0; i < InventorySlots.Count; i++)
+        {
+            Item heldItem = InventorySlots[i].getItem();
+
+            if (heldItem != null && itemToAdd.name == heldItem.name)
+            {
+                int freeSpaceInSlot = heldItem.maxQuantity - heldItem.currentQuantity;
+
+                if (freeSpaceInSlot >= leftoverQuantity)
+                {
+                    heldItem.currentQuantity += leftoverQuantity;
+                    Destroy(itemToAdd.gameObject);
+                    InventorySlots[i].updateData();
+                    return;
+                }
+                else // Add as much as we can to the current slot
+                {
+                    heldItem.currentQuantity = heldItem.maxQuantity;
+                    leftoverQuantity -= freeSpaceInSlot;
+                }
+            }
+            else if (heldItem == null)
+            {
+                if (!openSlot)
+                    openSlot = InventorySlots[i];
+            }
+
+            InventorySlots[i].updateData();
+        }
+
+        if (leftoverQuantity > 0 && openSlot)
+        {
+            openSlot.setItem(itemToAdd);
+            itemToAdd.currentQuantity = leftoverQuantity;
+            itemToAdd.gameObject.SetActive(false);
+        }
+        else
+        {
+            itemToAdd.currentQuantity = leftoverQuantity;
+        }
+    }
+
+    private void toggleInventory(bool enable)
+    {
+        inventory.SetActive(enable);
+
+        Cursor.lockState = enable ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = enable;
+
+        // Disable the rotation of the camera.
+      
+    }
+}
--- Project_Game_Jam/Assets/scripts/PlayerMovement.cs
+++ Project_Game_Jam/Assets/scripts/PlayerMovement.cs
@@ -1,52 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] 
-    CharacterController characterController;
+    /*[SerializeField]
+    CharacterController characterController;*/
     [SerializeField]
-    float Speed = 10f;
+    float Speed;
+    public float SprintSpeed;
+    public float trueSpeed;
     [SerializeField]
     float smoothTime = 0.1f;
     [SerializeField]
     float smoothVelocity;
     [SerializeField]
     Transform cam;
+    [Header("Gravity And Jump")]
     [SerializeField]
     float Gravity = -7.5f;
     [SerializeField]
     Vector3 velocity;
+    [SerializeField]
+    float JumpSpeed = 3f;
+    [SerializeField]
+    Vector3 force;
+    public bool isJumping;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        CharacterController characterController = GetComponent<CharacterController>();
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float Vertical = Input.GetAxisRaw("Vertical");    
-        Vector3 Deraction = new Vector3(horizontal , 0 , Vertical).normalized;
+        float Vertical = Input.GetAxisRaw("Vertical");
+        Vector3 Deraction = new Vector3(horizontal, 0, Vertical).normalized;
 
         if (Deraction.magnitude >= 0.1f)
         {
             float targetAngel = Mathf.Atan2(Deraction.x, Deraction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angel = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngel, ref smoothVelocity,smoothTime);
+            float angel = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngel, ref smoothVelocity, smoothTime);
             transform.rotation = Quaternion.Euler(0f, angel, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f,targetAngel, 0f) * Vector3.forward;
-            characterController.Move(moveDir.normalized * Speed * Time.deltaTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngel, 0f) * Vector3.forward;
+            characterController.Move(moveDir.normalized * trueSpeed * Time.deltaTime);
+
         }
+
+
 
         if (!characterController.isGrounded)
         {
             velocity.y += Gravity * Time.deltaTime;
             characterController.Move(velocity);
         }
+
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            velocity.y = 0f * Time.deltaTime;
+            characterController.Move(force);
+        }
+        isJumping = true;
+
+       
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            trueSpeed = SprintSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            trueSpeed = Speed; ;
+        }
+
+
     }
-    
 }

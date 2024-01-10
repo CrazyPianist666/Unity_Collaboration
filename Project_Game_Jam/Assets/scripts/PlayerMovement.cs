18a84b93f6198e4f4c5a7b5bb1453b5b78d9416e
Ashok0010001 <ashokmanivannan3@gmail.com>
2024-01-10 19:26:15 +0530

    Convert to FPS and gravity

--- Project_Game_Jam/Assets/scripts/PlayerMovement.cs
+++ Project_Game_Jam/Assets/scripts/PlayerMovement.cs
@@ -6,82 +6,69 @@ using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    /*[SerializeField]
-    CharacterController characterController;*/
+    [Header("PlayerMovement")]
     [SerializeField]
     float Speed;
-    public float SprintSpeed;
     public float trueSpeed;
-    [SerializeField]
-    float smoothTime = 0.1f;
-    [SerializeField]
-    float smoothVelocity;
-    [SerializeField]
-    Transform cam;
+    public float currentSpeed;
+    public CharacterController characterController;
     [Header("Gravity And Jump")]
-    [SerializeField]
-    float Gravity = -7.5f;
-    [SerializeField]
-    Vector3 velocity;
-    [SerializeField]
-    float JumpSpeed = 3f;
-    [SerializeField]
-    Vector3 force;
-    public bool isJumping;
+    public float gravity = -9.81f;
+    public Vector3 velocity;
+    public Transform GroundCheck;
+    public bool isGrounded;
+    public LayerMask groundMask;
+    public float groundDist = 0.4f;
+    public float JumpSpeed = 3f;
+    
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+       
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        CharacterController characterController = GetComponent<CharacterController>();
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float Vertical = Input.GetAxisRaw("Vertical");
-        Vector3 Deraction = new Vector3(horizontal, 0, Vertical).normalized;
 
-        if (Deraction.magnitude >= 0.1f)
+        //GroundCheck And Gravity
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDist, groundMask);
+        if (isGrounded && velocity.y < 0)
         {
-            float targetAngel = Mathf.Atan2(Deraction.x, Deraction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angel = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngel, ref smoothVelocity, smoothTime);
-            transform.rotation = Quaternion.Euler(0f, angel, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngel, 0f) * Vector3.forward;
-            characterController.Move(moveDir.normalized * trueSpeed * Time.deltaTime);
-
+            velocity.y = -2f;       
         }
 
 
+        //PlayerMovement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        if (!characterController.isGrounded)
+        Vector3 Move = transform.right * x + transform.forward * z;
+        characterController.Move(Move * trueSpeed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
+
+        //jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y += Gravity * Time.deltaTime;
-            characterController.Move(velocity);
+            velocity.y = Mathf.Sqrt(JumpSpeed * -2f * gravity);
         }
 
-        if (Input.GetButtonDown("Jump") && isJumping == false)
-        {
-            velocity.y = 0f * Time.deltaTime;
-            characterController.Move(force);
-        }
-        isJumping = true;
 
-       
+        //Sprint
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            trueSpeed = SprintSpeed;
+            trueSpeed = currentSpeed;
         }
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            trueSpeed = Speed; ;
+            trueSpeed = Speed;
         }
-
 
     }
 }

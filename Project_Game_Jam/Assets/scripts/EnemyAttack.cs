3836e8de372e3cd3a7f90b906b0b7485989a6a76
Ashok0010001 <ashokmanivannan3@gmail.com>
2024-01-31 22:49:25 +0530

    Animation Added

--- Project_Game_Jam/Assets/scripts/EnemyAttack.cs
+++ Project_Game_Jam/Assets/scripts/EnemyAttack.cs
@@ -15,7 +15,7 @@ public class EnemyAttack : MonoBehaviour
     {
         if (PlayerDetection.PlayerOnRange)
         {
-            print("On Range");
+            
         }
     }
 }
--- Project_Game_Jam/Assets/scripts/PlayerMovement.cs
+++ Project_Game_Jam/Assets/scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,11 +7,18 @@ using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Animations")]
+    private Animator animator;
+    private int xAnimId;
+    private int zAnimId;
+    Vector2 CurrentAnimBlendVector;
+    Vector2 animationVelocity;
+    float animationSmooth = 0.05f;
+    float animTrans = 0.15f;
+    int JumpAnim;
     [Header("PlayerMovement")]
     [SerializeField]
     float Speed;
-    public float trueSpeed;
-    public float currentSpeed;
     public CharacterController characterController;
     [Header("Gravity And Jump")]
     public float gravity = -9.81f;
@@ -20,21 +28,24 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundMask;
     public float groundDist = 0.4f;
     public float JumpSpeed = 3f;
-    
+   
 
 
 
-    // Start is called before the first frame update
-    void Start()
+
+   
+    void Awake()
     {
-       
-
+       animator = GetComponent<Animator>();
+        xAnimId = Animator.StringToHash("MoveX");
+        zAnimId = Animator.StringToHash("MoveZ");
+        JumpAnim = Animator.StringToHash("Jump");
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-
+        
         //GroundCheck And Gravity
         isGrounded = Physics.CheckSphere(GroundCheck.position, groundDist, groundMask);
         if (isGrounded && velocity.y < 0)
@@ -46,29 +57,34 @@ public class PlayerMovement : MonoBehaviour
         //PlayerMovement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        Vector3 plmove = new Vector3(x, z, 0);
 
-        Vector3 Move = transform.right * x + transform.forward * z;
-        characterController.Move(Move * trueSpeed * Time.deltaTime);
+        CurrentAnimBlendVector = Vector2.SmoothDamp(CurrentAnimBlendVector,plmove,ref animationVelocity,animationSmooth);
+
+        Vector3 Move = transform.right * CurrentAnimBlendVector.x + transform.forward * CurrentAnimBlendVector.y;
+        characterController.Move(Move * Speed * Time.deltaTime);
         velocity.y += gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
 
+        
 
         //jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+           
             velocity.y = Mathf.Sqrt(JumpSpeed * -2f * gravity);
+            animator.CrossFade(JumpAnim, animTrans);
+            
         }
 
 
-        //Sprint
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            trueSpeed = currentSpeed;
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            trueSpeed = Speed;
-        }
+
+        //Animation
+        animator.SetFloat(xAnimId, Move.x);
+        animator.SetFloat(zAnimId, Move.z);
+
+       
+
 
     }
 }

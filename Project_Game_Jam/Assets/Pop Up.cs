7dcd249b7793521e8520b47d133ae823c59e0e4c
CrazyPianist666 <151561340+CrazyPianist666@users.noreply.github.com>
2024-02-03 15:16:28 +0530

    Popup Window

---
+++ Project_Game_Jam/Assets/Pop Up.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    public GameObject Panel;
+    public bool isTrigger;
+
+    void Start()
+    {
+        Panel.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isTrigger = true;
+            Panel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isTrigger = false;
+            Panel.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
